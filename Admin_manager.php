809da502dc3e3f05a709949bdba8f1b80368df6f
Mashrur Jamil Rafin <mashrur.jamil@northsouth.edu>
2021-08-19 20:27:11 +0600

    Admin Manager

---
+++ Admin_manager.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+   
+   <!-- <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"></script>
+       -->
+      <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+     
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+      
+    
+    <style>
+
+
+    </style>
+
+</head>
+<body>
+
+    
+    <div class="container-fluid bg-dark" style="padding-bottom: 100px;">
+        <div class="team-table bg-dark text-warning">
+         <div class="title text-center mb-1">
+             <h3 class= "font-weight bolder py-5">Admin Manager</h3>
+         </div>
+     
+     <table class="table table-bordered text-center text-white">
+         <thead class="thead">
+             <tr>
+     
+                 <th>ID</th>
+                 <th>Full Name</th>
+                 <th>Username</th>
+                 <th>Actions</th>  
+             </tr>
+         </thead>
+         <tbody>
+                 <tr>
+                         <td>1</td>
+                         <td>Mashrur Rafin</td>
+                         <td>rafin12</td>
+                         <td class="float-left">
+                             <a class="btn btn-outline-warning" href="">Update Admin</a>
+                             <a class="btn btn-outline-danger" href="">Delete Admin</a>
+                         </td>
+     
+                 </tr>
+                 <tr>
+                    <td>2</td>
+                    <td>Rahemul Islam</td>
+                    <td>rahem45</td>
+                    <td class="float-left">
+                        <a class="btn btn-outline-warning" href="">Update Admin</a>
+                        <a class="btn btn-outline-danger" href="">Delete Admin</a>
+                    </td>
+
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Mahfuj Jim</td>
+                <td>jim78</td>
+                <td class="float-left">
+                    <a class="btn btn-outline-warning" href="">Update Admin</a>
+                    <a class="btn btn-outline-danger" href="">Delete Admin</a>
+                </td>
+        </tr>
+                
+         </tbody>
+     </table>
+     <div class="text-center"><a class="btn btn-outline-success" href="">Add Admin</a></div>
+    </div>
+   </div> 
+
+
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
